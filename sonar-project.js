4a0b14d132a34069f34a470b0428b1a0f1516862
ADJIN-TETTEY <127889126+ADJIN-TETTEY@users.noreply.github.com>
2023-05-13 12:17:58 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '4ba11272d13b72abcec2c5fa14bff2e49fe1822b',
+	    'sonar.login': '9d75e4518143560ea4f19c4ae9959b520fd8937c',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
